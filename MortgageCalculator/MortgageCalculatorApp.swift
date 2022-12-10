81aed88f792c4bec6e475d072dcb699592f485ff
Bill Wong <billwong7788@gmail.com>
2022-12-10 19:44:51 +1100

    Initial Commit

---
+++ MortgageCalculator/MortgageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageCalculatorApp.swift
+//  MortgageCalculator
+//
+//  Created by Bill Wong on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MortgageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
